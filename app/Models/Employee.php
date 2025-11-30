fddac7ce6c9df806676bfef780db12a1c7db80bf
unknown <hcphfo.espencer.liparanon@gmail.com>
2025-11-30 09:32:33 +0800

    fix the error line in employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Employee extends Model
 {
@@ -20,6 +21,7 @@ class Employee extends Model
         'role',
         'department_id', 
         'user_id',
+        'employee_id',
     ];
 
     // /**
